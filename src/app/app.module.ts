c92e1ba28e73fb64f6bdcb2de6ebcd5e67861f6a
jonolds <jonolds@gmail.com>
2019-06-19 05:32:08 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { TeaListComponent } from './tea-list/tea-list.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: TeaListComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    TeaListComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
