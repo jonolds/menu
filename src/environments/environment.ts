421f95b2d51dc7a70506e39f9b2d5dca6ec5bd33
jonolds <jonolds@gmail.com>
2019-06-23 12:44:21 -0500

    win

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false
+  production: false
 };
 
 /*
@@ -13,11 +13,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';	// Included with Angular CLI.
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
